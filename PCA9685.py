ed7598b5ba578495987a485a45e5838e97e2c65a
Serdar Üşenmez <serdar.usenmez@gmail.com>
2020-02-16 16:00:03 +0300

    Modified PCA9685 driver to accept pulse width for any frequency. Modified PCA9685 driver to use auto-increment write to update all nine servo pulse widths at once.

--- PCA9685.py
+++ PCA9685.py
@@ -2,12 +2,7 @@ import time
 import math
 import smbus
 
-# ============================================================================
-# Raspi PCA9685 16-Channel PWM Servo Driver
-# ============================================================================
-
 class PCA9685:
-    # Registers/etc.
     __SUBADR1            = 0x02
     __SUBADR2            = 0x03
     __SUBADR3            = 0x04
@@ -22,60 +17,52 @@ class PCA9685:
     __ALLLED_OFF_L       = 0xFC
     __ALLLED_OFF_H       = 0xFD
 
-    def __init__(self, address=0x40, debug=False):
+    leg_pulse_data = [0 for _ in range(12)]
+    pulse_scale = 4096 / 20000
+
+    def __init__(self, address):
         self.bus = smbus.SMBus(1)
         self.address = address
-        self.debug = debug
-        if (self.debug):
-            print("Reseting PCA9685")
 
         self.write(self.__MODE1, 0x00)
 
     def write(self, reg, value):
-        "Writes an 8-bit value to the specified register/address"
         self.bus.write_byte_data(self.address, reg, value)
-        if (self.debug):
-            print("I2C: Write 0x%02X to register 0x%02X" % (value, reg))
 
     def read(self, reg):
-        "Read an unsigned byte from the I2C device"
-        result = self.bus.read_byte_data(self.address, reg)
-        if (self.debug):
-            print("I2C: Device 0x%02X returned 0x%02X from reg 0x%02X" % (self.address, result & 0xFF, reg))
+        return self.bus.read_byte_data(self.address, reg)
 
-        return result
+    def set_frequency(self, frequency):
+        self.pulse_scale = 4096 / (1000000 / frequency)
+        prescale = round(25000000 / (4096 * frequency)) - 1
 
-    def setPWMFreq(self, freq):
-        "Sets the PWM frequency"
-        prescaleval = 25000000.0    # 25MHz
-        prescaleval /= 4096.0       # 12-bit
-        prescaleval /= float(freq)
-        prescaleval -= 1.0
-        if (self.debug):
-            print("Setting PWM frequency to %d Hz" % freq)
-            print("Estimated pre-scale: %d" % prescaleval)
-        prescale = math.floor(prescaleval + 0.5)
-        if (self.debug):
-            print("Final pre-scale: %d" % prescale)
+        mode1_prev = self.read(self.__MODE1);
 
-        oldmode = self.read(self.__MODE1);
-        newmode = (oldmode & 0x7F) | 0x10        # sleep
-        self.write(self.__MODE1, newmode)        # go to sleep
-        self.write(self.__PRESCALE, int(math.floor(prescale)))
-        self.write(self.__MODE1, oldmode)
+        # Sleep
+        mode1 = (mode1_prev & 0x7F) | 0x10
+        self.write(self.__MODE1, mode1)
+        self.write(self.__PRESCALE, int(prescale))
+        self.write(self.__MODE1, mode1_prev)
+
+        # Wait for oscillator
         time.sleep(0.005)
-        self.write(self.__MODE1, oldmode | 0x80)
 
-    def setPWM(self, channel, on, off):
-        "Sets a single PWM channel"
-        self.write(self.__LED0_ON_L+4*channel, on & 0xFF)
-        self.write(self.__LED0_ON_H+4*channel, on >> 8)
-        self.write(self.__LED0_OFF_L+4*channel, off & 0xFF)
-        self.write(self.__LED0_OFF_H+4*channel, off >> 8)
-        if (self.debug):
-            print("channel: %d  LED_ON: %d LED_OFF: %d" % (channel,on,off))
+        # Restart
+        self.write(self.__MODE1, mode1_prev | 0x80)
 
-    def setServoPulse(self, channel, pulse):
-        "Sets the Servo Pulse,The PWM frequency must be 50HZ"
-        pulse = pulse*4096/20000        #PWM frequency is 50HZ,the period is 20000us
-        self.setPWM(channel, 0, int(pulse))
+    def write_pulses(self, pulses):
+        self.bus.write_byte_data(self.address, self.__MODE1, 0b00100000)
+
+        for leg in range(3):
+            pulse1 = int(pulses[(3 * leg) + 0] * self.pulse_scale)
+            pulse2 = int(pulses[(3 * leg) + 1] * self.pulse_scale)
+            pulse3 = int(pulses[(3 * leg) + 2] * self.pulse_scale)
+            self.leg_pulse_data[2] = pulse1 & 0xff
+            self.leg_pulse_data[3] = pulse1 >> 8
+            self.leg_pulse_data[6] = pulse2 & 0xff
+            self.leg_pulse_data[7] = pulse2 >> 8
+            self.leg_pulse_data[10] = pulse3 & 0xff
+            self.leg_pulse_data[11] = pulse3 >> 8
+            self.bus.write_i2c_block_data(self.address, self.__LED0_ON_L + (leg * 12), self.leg_pulse_data)
+
+        self.bus.write_byte_data(self.address, self.__MODE1, 0)
